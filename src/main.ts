75df36348194aa2ac5fc6b3e6962f4b679537a9b
s4j4d <mmmsms97@gmail.com>
2024-12-22 00:09:35 +0330

    Feat(add credentials header for the CORS)

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const logger = new Logger(bootstrap.name);
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   //TODO: remove this later
-  app.enableCors({ origin: 'https://menyou-spa.darkube.app' });
+  app.enableCors({
+    origin: 'https://menyou-spa.darkube.app',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.set('trust proxy', true);
   app.use(cookieParser());
